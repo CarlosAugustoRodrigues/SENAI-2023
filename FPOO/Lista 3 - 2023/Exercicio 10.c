69f142b1b66fd48383cec4c0ac3b4f1857c11197
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-08-24 09:00:30 -0300

    SENAI

---
+++ FPOO/Lista 3 - 2023/Exercicio 10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "");
+	
+	int n[10], maior, i;
+	
+	printf("Digite 10 números inteiros:\n");
+	
+	for(i = 0; i < 10; i++){
+		scanf("\n%d", &n[i]);
+	}
+	
+	for(i = 0; i < 10; i++){
+		if(n[i] > n[i + 1]){
+			maior = n[i];
+		}else{
+			maior = n[i + 1];
+		}
+	}
+	
+	printf("\n\nO maior número da sequência digitada é: %d", maior);
+	
+	return 0;
+}
---
+++ FPOO/Lista 3 - 2023/Exercicio 11.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	
+	
+	setlocale(LC_ALL, "");
+	
+	int n[5], contador, i, aux;
+	
+	printf("Digite 5 números inteiros em qualquer ordem:\n");
+	for(contador = 0; contador < 5; contador++){
+		scanf("\n%d", &n[contador]);
+	}
+	
+	printf("\nNúmeros não ordenados: ");
+	for(contador = 0; contador < 5; contador++){
+		printf("%d ", n[contador]);
+	}
+	
+	for(contador = 0; contador < 5; contador++){
+		for(i = 0; i < 5; i++){
+			if(n[i] > n[i+1]){
+				aux = n[i];
+				n[i] = n[i+1];
+				n[i+1] = aux;
+			}
+		}
+	}
+	
+	printf("\n\nNúmeros em ordem crescente: ");
+	for(contador = 0; contador < 5; contador++){
+		printf("%d ", n[contador]);
+	}
+	return 0;
+}
