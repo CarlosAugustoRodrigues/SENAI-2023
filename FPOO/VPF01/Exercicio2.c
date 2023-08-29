3018ce68adeae13b13709dc287a6dc8c6d704d48
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-08-29 09:17:23 -0300

    SENAI

---
+++ FPOO/VPF01/Exercicio2.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "");
+	
+	char cidade[20];
+	int candidatos, i, soma = 0;
+	
+	printf("Cidade: ");
+	scanf("%s", &cidade);
+	
+	printf("Quantidade de candidatos: ");
+	scanf("%d", &candidatos);
+	
+	char nome[candidatos][20];
+	int votosB, votosN, votosCandi[candidatos];
+	
+	printf("\n");
+	printf("Digite as seguintes informações dos candidatos");
+	printf("\n");
+	printf("______________________________________________");
+	printf("\n");
+	
+	for(i = 0; i < candidatos; i++){
+		printf("%dº candidato:", i + 1);
+		printf("\n");
+		printf("Nome: ");
+		scanf("%s", &nome[i]);
+		printf("Nº de votos: ");
+		scanf("%d", &votosCandi[i]);
+		printf("\n");
+	}
+	
+	printf("Votos Brancos: ");
+	scanf("%d", &votosB);
+	printf("Votos Nulos:");
+	scanf("%d", &votosN);
+	
+	for(i = 0; i < candidatos; i++){
+		soma+=votosCandi[i];
+	}
+	
+	int somaTotal = soma + votosB;
+	
+	printf("\n");
+	printf("\n");
+	
+	printf("Situação da Eleição: ");
+	if(somaTotal > votosN){
+		printf("Valida");
+		printf("\n");
+		printf("Votos Válidos: %d", somaTotal);
+		printf("\n");
+		printf("Votos Inválidos: %d", votosN);
+		printf("\n");
+		printf("______________________________________________");
+		printf("\n");
+		
+		float porcentagemVotos[candidatos];
+		
+		printf("\n");
+		printf("Candidatos\t\t%% de votos");
+		printf("\n");
+		
+		for(i = 0; i < candidatos; i++){
+			printf("%s\t\t\t%.1f%%", nome[i], porcentagemVotos[i] = (votosCandi[i] * 100)/somaTotal);
+			printf("\n");
+		}
+		
+		if(somaTotal > 200000){
+			
+		}else{
+			printf("Não haverá 2º Turno");
+		}
+	}else{
+		printf("Inválida");
+	}
+
+	
+	return 0;
+}
