e495ae9a6ea9f06c94dbeec378d805f6ada865f8
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-08-31 13:08:59 -0300

    SENAI

--- FPOO/VPF01/Exercicio2.c
+++ FPOO/VPF01/Exercicio2.c
@@ -18,24 +18,18 @@ int main(){
 	char nome[candidatos][20];
 	int votosB, votosN, votosCandi[candidatos];
 	
-	printf("\n");
-	printf("Digite as seguintes informações dos candidatos");
-	printf("\n");
-	printf("______________________________________________");
-	printf("\n");
-	printf("\n");
+	printf("\nDigite as seguintes informações dos candidatos\n");
+	printf("______________________________________________\n\n");
 	
 	for(i = 0; i < candidatos; i++){
 		printf("%dº candidato:", i + 1);
-		printf("\n");
-		printf("Nome: ");
+		printf("\nNome: ");
 		scanf("%s", &nome[i]);
 		printf("Nº de votos: ");
 		scanf("%d", &votosCandi[i]);
-		printf("\n");
 	}
 	
-	printf("Votos Brancos: ");
+	printf("\nVotos Brancos: ");
 	scanf("%d", &votosB);
 	printf("Votos Nulos:");
 	scanf("%d", &votosN);
@@ -46,74 +40,57 @@ int main(){
 	
 	float votosValidos = soma + votosB;
 	
-	printf("\n");
-	printf("______________________________________________");
-	printf("\n");
-	printf("\n");
-	
-	printf("Situação da Eleição: ");
+	printf("\n______________________________________________\n");
+
+	printf("\nSituação da Eleição: ");
 	
 	if(votosValidos > votosN){
-		printf("Valida");
-		printf("\n");
-		printf("Votos Válidos: %.0f", votosValidos);
-		printf("\n");
-		printf("Votos Inválidos: %d", votosN);
-		printf("\n");
-		printf("\n");
+		printf("Valida\n");
+		printf("Votos Válidos: %.0f\n", votosValidos);
+		printf("Votos Inválidos: %d\n\n", votosN);
 		
 		float porcentagemVotos[candidatos];
 		
-		printf("\n");
-		printf("Candidatos");
+		printf("\nCandidatos");
 		printf("\t\t");
-		printf("%% de votos");
-		printf("\n");
+		printf("%% de votos\n");
 		
 		for(i = 0; i < candidatos; i++){
 			printf("%s", nome[i]);
 			printf("\t\t\t");
-			printf("%.2f%%", porcentagemVotos[i] = (votosCandi[i] * 100)/votosValidos);
-			printf("\n");
+			printf("%.2f%%\n", porcentagemVotos[i] = (votosCandi[i] * 100)/votosValidos);
 		}
 		
 		float porcentagemVB;
 		
 		printf("Votos Brancos");
 		printf("\t\t");
-		printf("%.2f%%", porcentagemVB = (votosB * 100)/votosValidos);
-		printf("\n");
+		printf("%.2f%%\n", porcentagemVB = (votosB * 100)/votosValidos);
 		
-		int primeiro;
-		int segundo;
+		int auxVotos;
+		char auxNome[20];
 		int j;
 		
 		for(i = 0; i < candidatos; i++){
-			for(j = 0; j < candidatos - 1; j++){
-				if(votosCandi[j] > votosCandi[j + 1]){
-					primeiro = j;
+			for(j = i + 1; j < candidatos; j++){
+				if(votosCandi[i] < votosCandi[j]){
+					auxVotos = votosCandi[i];
+					strcpy(auxNome, nome[i]);
+					votosCandi[i] = votosCandi[j];
+					strcpy(nome[i], nome[j]);
+					votosCandi[j] = auxVotos;
+					strcpy(nome[j], auxNome);
 				}
-			}
-			for(j = 0; j < candidatos - 1; j++){
-				if(votosCandi[j] > votosCandi[j + 1]){
-					if()
-					segundo = j;
-				}	
 			}
 		}
 		
-		if(votosValidos > 200000 && votosCandi[primeiro] < (votosValidos / 2) + 1){
-			printf("\n");
-			printf("Haverá 2º turno entre:");
-			printf("\n");
-			printf("Candidato: %s\tVotos: %d", nome[primeiro], votosCandi[primeiro]);
-			printf("\n");
-			printf("Candidato: %s\tVotos: %d", nome[segundo], votosCandi[segundo]);
+		if(votosValidos > 200000 && votosCandi[0] < (votosValidos / 2) + 1){
+			printf("\nHaverá 2º turno entre:");
+			printf("\nCandidato: %s\tVotos: %d", nome[0], votosCandi[0]);
+			printf("\nCandidato: %s\tVotos: %d", nome[1], votosCandi[1]);
 		}else{
-			printf("\n");
-			printf("Não haverá 2º turno");
-			printf("\n");
-			printf("Candidato Eleito: %s", nome[primeiro]);
+			printf("\nNão haverá 2º turno");
+			printf("\nCandidato Eleito: %s", nome[0]);
 		}
 	}else{
 		printf("Inválida");
