82f2a02f7dcf19a957343a9c90c88822474a47fc
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-09-04 08:47:22 -0300

    SENAI

---
+++ FPOO/Lista 4 - 2023/Exercicio3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "");
+	
+	char mercadoria[5][20];
+	float preco[5];
+	int i;
+	
+	for(i = 0; i < 5; i++){
+		printf("\nNome da mercadoria: ");
+		scanf("%s", &mercadoria);
+		printf("Preço da mercadoria: ");
+		scanf("%f", &preco[i]);
+	}
+	
+	float precoReajustado;
+	
+	for(i = 0; i < 5; i++){
+		if(preco < 1000){
+			precoReajustado[i] = preco[i] * 1.05;
+		}else{
+			precoReajustado[i] = preco[i] * 1.07;
+		}
+	}	
+	
+	return 0;
+}
