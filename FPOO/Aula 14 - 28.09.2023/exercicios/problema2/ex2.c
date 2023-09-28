e0110cfe850406a2e4a8786d08ebd9dc37e711b9
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-09-28 08:37:26 -0300

    SENAI

--- FPOO/Aula 14 - 28.09.2023/exercicios/problema2/ex2.c
+++ FPOO/Aula 14 - 28.09.2023/exercicios/problema2/ex2.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-// Função para calcular a direção final com base nos comandos
-char calcular_direcao_final(char comandos[], int N) {
-    char direcao = 'N';  // Inicialmente voltado para o norte
+char final(char comandos[], int N) {
+    char direcao = 'N';
     
     int i;
 
     for (i = 0; i < N; i++) {
         if (comandos[i] == 'E') {
-            // Girar à esquerda
             if (direcao == 'N') {
                 direcao = 'O';
             } else if (direcao == 'O') {
@@ -19,7 +17,6 @@ char calcular_direcao_final(char comandos[], int N) {
                 direcao = 'N';
             }
         } else if (comandos[i] == 'D') {
-            // Girar à direita
             if (direcao == 'N') {
                 direcao = 'L';
             } else if (direcao == 'L') {
@@ -39,14 +36,12 @@ int main() {
     int N;
     char comandos[1001];
 
-    // Abrir o arquivo de entrada
     FILE *entrada = fopen("esquerda.in.txt", "r");
     if (entrada == NULL) {
         perror("Erro ao abrir o arquivo de entrada");
         return 1;
     }
 
-    // Abrir o arquivo de saída
     FILE *saida = fopen("esquerda.out.txt", "w");
     if (saida == NULL) {
         perror("Erro ao abrir o arquivo de saída");
@@ -54,20 +49,18 @@ int main() {
         return 1;
     }
 
-    // Ler e processar os casos de teste
     while (1) {
         fscanf(entrada, "%d", &N);
         if (N == 0) {
-            break;  // Fim da entrada
+            break;
         }
 
         fscanf(entrada, "%s", comandos);
 
-        char direcao_final = calcular_direcao_final(comandos, N);
+        char direcao_final = final(comandos, N);
         fprintf(saida, "%c\n", direcao_final);
     }
 
-    // Fechar os arquivos
     fclose(entrada);
     fclose(saida);
 
