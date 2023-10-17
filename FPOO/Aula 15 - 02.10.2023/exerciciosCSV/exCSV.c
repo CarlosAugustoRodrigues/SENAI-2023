c7b2cb1d10fb0607d35c7498669992ade792c985
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-10-16 23:05:57 -0300

    SENAI

--- FPOO/Aula 15 - 02.10.2023/exerciciosCSV/exCSV.c
+++ FPOO/Aula 15 - 02.10.2023/exerciciosCSV/exCSV.c
@@ -3,26 +3,46 @@
 #include <string.h>
 
 int main() {
-    FILE *entrada;
+    FILE *entrada, *saida;
     entrada = fopen("entrada.csv", "r");
+    saida = fopen("saida.csv", "w");
     char linha[100];
 
     if (entrada == NULL) {
         printf("Erro ao abrir o arquivo");
         return 1;
     }
-
+    
+    fprintf(saida, "id;nome;nascimento;idade\n");
+    
+    //Cabeçalho
+    fgets(linha, sizeof(linha), entrada);
+    	char *hId, *hNome, *hNascimento;
+    	hId = strtok(linha, ";");
+    	hNome = strtok(NULL, ";");
+    	hNascimento = strtok(NULL, ";");
+    	
+    	printf("%s;%s;%s", hId, hNome, hNascimento);
+    
+    //Linhas
     while (fgets(linha, sizeof(linha), entrada) != NULL) {
-        char *id, *nome, *nascimento;
+        char *numId, *nome;
+        int ano, mes, dia;
         
-        id = strtok(linha, ";");
+        numId = strtok(linha, ";");
         nome = strtok(NULL, ";");
-        nascimento = strtok(NULL, ";");        
+        ano = atoi(strtok(NULL, "-"));        
+        mes = atoi(strtok(NULL, "-"));     
+        dia = atoi(strtok(NULL, ";"));   
+		
+		int idade = 2023 - ano;  
         
-        printf("%s %s %s", id, nome, nascimento);
+        printf("%s;%s;%d-%d-%d\n", numId, nome, ano, mes, dia);
+        fprintf(saida, "%s;%s;%d-%d-%d;%d\n", numId, nome, ano, mes, dia, idade);
     }
 
     fclose(entrada);
+    fclose(saida);
 
     return 0;
 }
