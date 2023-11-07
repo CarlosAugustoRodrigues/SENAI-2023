db48b0a8dad1823c121b4498626f8e915cdac1b7
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-11-07 10:16:27 -0300

    SENAI

--- PORTFOLIO/assets/script/script.js
+++ PORTFOLIO/assets/script/script.js
@@ -1,11 +1,34 @@
-var menuContainer = document.getElementById("menu");
+const sections = document.querySelectorAll("section");
+const menuLinks = document.querySelectorAll(".item-menu");
 
-var itens_menu = menuContainer.getElementsByClassName("item-menu");
+function isSectionInView(section) {
+  const rect = section.getBoundingClientRect();
+  return rect.top <= 150 && rect.bottom >= 150;
+}
 
-for (var i = 0; i < itens_menu.length; i++) {
-  itens_menu[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+function updateMenu() {
+  sections.forEach((section, index) => {
+    if (isSectionInView(section)) {
+      menuLinks.forEach((link) => link.classList.remove("active"));
+      menuLinks[index].classList.add("active");
+    }
   });
 }
+
+window.addEventListener("scroll", updateMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop - 100,
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+updateMenu();
