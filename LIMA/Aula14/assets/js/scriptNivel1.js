a1683c5164752630fbc85cc0208979675bb28f3d
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-12-07 23:40:51 -0300

    SENAI

---
+++ LIMA/Aula14/assets/js/scriptNivel1.js
@@ -0,0 +1,12 @@
+function ex01(){
+    window.alert('Hello World')
+}
+
+function ex02(){
+    window.alert('Você Clicou no Botão')
+}
+
+function ex03(){
+    let name = window.prompt('Qual o seu nome?')
+    window.alert(`Olá ${name}, é um prazer te conhecer!`)
+}
