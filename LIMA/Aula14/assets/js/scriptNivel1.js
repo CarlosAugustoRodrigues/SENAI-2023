d7ecf1e15b61c48c47f24adeec52dcbdcf7ffc3e
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2023-12-11 06:32:06 -0300

    SENAI

--- LIMA/Aula14/assets/js/scriptNivel1.js
+++ LIMA/Aula14/assets/js/scriptNivel1.js
@@ -2,15 +2,20 @@ const result = document.getElementById("result")
 var resultEx = document.createElement('div')
 
 function ex01(){
-    result.innerHTML = ""
-
-    result.innerHTML = `<h2 style="font-size: 70px">HELLO WORLD!</h2>`
+    window.alert('Hello World!')
 }
 
 function ex02(){
-    result.innerHTML = ""
+    result.innerHTML = `<h2>CLIQUE NO BOTÃO</h2>`
 
-    result.innerHTML = `<h2 style="font-size: 70px">VOCÊ CLICOU NO BOTÃO</h2>`
+    let btn = document.createElement('button')
+    btn.style.width = "170px"
+    btn.style.height = "50px"
+    btn.innerHTML = "CLIQUE AQUI"
+    btn.addEventListener('click', () => {
+        window.alert('Você clicou no botão')
+    })
+    result.appendChild(btn)
 }
 
 function ex03(){
@@ -30,6 +35,7 @@ function ex03(){
     result.appendChild(btn)
 
     resultEx.classList.add('resultEx')
+    
     result.appendChild(resultEx)
 }
 
@@ -77,7 +83,7 @@ function ex05(){
 
 function ex06(){
     resultEx.innerHTML = ""
-    result.innerHTML = `<h2>DIGITE DOIS NÚMERO INTEIRO E POSITIVO PARA SOMAR</h2>`
+    result.innerHTML = `<h2>DIGITE DOIS NÚMERO INTEIRO PARA SOMAR</h2>`
 
     let input = document.createElement('input')
     result.appendChild(input)
@@ -119,12 +125,14 @@ function ex07(){
     btn.innerHTML = "ENVIAR"
     btn.addEventListener('click', () => {
         let name = input.value
-        let nota1 = Number(input2.value)
-        let nota2 = Number(input3.value)
-        resultEx.innerHTML = `<span style="font-size: 30px">Aluno: ${name}<span>`
+        let nota1 = input2.value.replace(',', '.')
+        let nota2 = input3.value.replace(',', '.')
+        nota1 = Number(nota1)
+        nota2 = Number(nota2)
+        resultEx.innerHTML = `<span style="font-size: 30px">Aluno(a): ${name}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">Nota 1: ${nota1}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">Nota 2: ${nota2}<span>`
-        resultEx.innerHTML += `<span style="font-size: 30px">Média: ${(nota1 + nota2) / 2}<span>`
+        resultEx.innerHTML += `<span style="font-size: 30px">Média: ${((nota1 + nota2) / 2).toFixed(2)}<span>`
         input.value = ""
         input2.value = ""
         input3.value = ""
@@ -145,18 +153,92 @@ function ex08(){
     let btn = document.createElement('button')
     btn.innerHTML = "ENVIAR"
     btn.addEventListener('click', () => {
-        let number = Number(input.value)
+        let number  = input.value.replace(',', '.')
+        number = Number(number)
         resultEx.innerHTML = `<span style="font-size: 30px">Número em análise: ${number}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">O seu valor absoluto é: ${Math.abs(number)}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">A sua parte inteira é: ${Math.trunc(number)}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">O valor inteiro mais próximo é: ${Math.round(number)}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">A sua raiz quadrada é = ${Math.sqrt(number).toFixed(2)}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">A sua raiz cúbica é = ${Math.cbrt(number).toFixed(2)}<span>`
-        resultEx.innerHTML += `<span style="font-size: 30px">O valor de ${number}² =  ${Math.pow(number, 2).toFixed(2)}<span>`
+        resultEx.innerHTML += `<span style="font-size: 30px>O valor de ${number}² =  ${Math.pow(number, 2).toFixed(2)}<span>`
         resultEx.innerHTML += `<span style="font-size: 30px">O valor de ${number}³ = ${Math.pow(number, 3).toFixed(2)}<span>`
         input.value = ""
     })
     result.appendChild(btn)
+
+    resultEx.classList.add('resultEx')
+    result.appendChild(resultEx)
+}
+
+function ex09(){
+    let clicks = 0
+
+    resultEx.innerHTML = ""
+    result.innerHTML = `<h2>CONTADOR DE CLIQUES</h2>`
+
+    let btn = document.createElement('button')
+    btn.style.width = "170px"
+    btn.style.height = "50px"
+    btn.innerHTML = "CLIQUE AQUI"
+    btn.addEventListener('click', () => {
+        clicks++
+        resultEx.innerHTML = `<span style="font-size: 30px">O botão foi clicado ${clicks} vez(es)<span>`
+    })
+    result.appendChild(btn)
+
+    let btn2 = document.createElement('button')
+    btn2.style.width = "170px"
+    btn2.style.height = "50px"
+    btn2.innerHTML = "LIMPAR CONTADOR"
+    btn2.addEventListener('click', () => {
+        clicks = 0
+        resultEx.innerHTML = `<span style="font-size: 30px">O botão foi clicado ${clicks} vez(es)<span>`
+    })
+    result.appendChild(btn2)
+
+    resultEx.classList.add('resultEx')
+    result.appendChild(resultEx)
+}
+
+function ex10(){
+
+    resultEx.innerHTML = ""
+    result.innerHTML = `<h2>REGISTRANDO SUAS AÇÕES COM OS BOTÕES ACIMA</h2>`
+
+    let divButton = document.createElement('div')
+    divButton.style.display = "flex"
+    divButton.style.columnGap = "15px"
+    divButton.classList.add('divButton')
+    result.appendChild(divButton)
+
+    let btn = document.createElement('button')
+    btn.innerHTML = "AÇÃO 1"
+    btn.addEventListener('click', () => {
+        resultEx.innerHTML += `<span style="font-size: 30px">Você clicou o primeiro botão<span>`
+    })
+    divButton.appendChild(btn)
+
+    let btn2 = document.createElement('button')
+    btn2.innerHTML = "AÇÃO 2"
+    btn2.addEventListener('click', () => {
+        resultEx.innerHTML += `<span style="font-size: 30px">Você clicou o segundo botão<span>`
+    })
+    divButton.appendChild(btn2)
+
+    let btn3 = document.createElement('button')
+    btn3.innerHTML = "AÇÃO 3"
+    btn3.addEventListener('click', () => {
+        resultEx.innerHTML += `<span style="font-size: 30px">Você clicou o terceiro botão<span>`
+    })
+    divButton.appendChild(btn3)
+
+    let btn4 = document.createElement('button')
+    btn4.innerHTML = "AÇÃO 4"
+    btn4.addEventListener('click', () => {
+        resultEx.innerHTML += `<span style="font-size: 30px">Você clicou o quarto botão<span>`
+    })
+    divButton.appendChild(btn4)
 
     resultEx.classList.add('resultEx')
     result.appendChild(resultEx)
---
+++ LIMA/Aula14/assets/js/scriptNivel2.js
@@ -0,0 +1,34 @@
+const result = document.getElementById('#result')
+var resultEx = document.createElement('div')
+
+function ex11(){
+    resultEx.innerHTML = ""
+
+    let btn = document.createElement('button')
+    btn.innerHTML = "CALCULAR MÉDIA"
+    btn.addEventListener('click', () => {
+        let name = prompt('Nome do aluno(a):')
+        let nota1 = prompt('Primeira nota:').replace(',', '.')
+        let nota2 = prompt('Segunda nota:').replace(',', '.')
+        nota1 = Number(nota1)
+        nota2 = Number(nota2)
+        let media = Number((nota1 + nota2) / 2).toFixed(2)
+
+        let msg
+        if (media >= 6) {
+            msg = 'Meus parabéns!'
+        } else {
+            msg = 'Estude um pouco mais!'
+        }
+
+        resultEx.innerHTML = `<span style="font-size: 30px">Aluno(a): ${name}<span><br>`
+        resultEx.innerHTML += `<span style="font-size: 30px">Nota 1: ${nota1}<span><br>`
+        resultEx.innerHTML += `<span style="font-size: 30px">Nota 2: ${nota2}<span><br>`
+        resultEx.innerHTML += `<span style="font-size: 30px">Média: ${media}<span><br>`
+        resultEx.innerHTML += `<span style="font-size: 30px">${msg}<span>`
+    })
+    result.appendChild(btn)
+
+    resultEx.classList.add('resultEx')
+    result.appendChild(resultEx)
+}
